e986d9d1cdf3336f713ad2d7fdd6e0a3f9eb0ad1
mikespub <github@mikespub.net>
2025-01-15 15:44:46 +0100

    remove dummy main

--- class/usergui.php
+++ class/usergui.php
@@ -39,17 +39,5 @@ sys::import('modules.calendar.class.userapi');
  */
 class UserGui extends UserGuiClass
 {
-    /**
-     * User main GUI function
-     * @param array<string, mixed> $args
-     * @return array<mixed>
-     */
-    public function main(array $args = [])
-    {
-        $args['description'] ??= 'Description of calendar';
-
-        // Pass along the context for xarTpl::module() if needed
-        $args['context'] ??= $this->getContext();
-        return $args;
-    }
+    // ...
 }
