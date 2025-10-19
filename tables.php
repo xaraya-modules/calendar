a3b0dbe6745e095acb7185308b148735be1be0e0
mikespub <github@mikespub.net>
2025-10-19 18:38:26 +0200

    add tables

---
+++ tables.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Xaraya\Modules\Calendar;
+
+use xarDB;
+
+class Tables
+{
+    /**
+     * Calendar Module
+     *
+     * @package modules
+     * @subpackage calendar module
+     * @category Third Party Xaraya Module
+     * @version 1.0.0
+     * @copyright (C) copyright-placeholder
+     * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+     * @author Marc Lutolf <mfl@netspan.ch>
+     */
+    public function __invoke(?string $prefix = null)
+    {
+        $xartables = [];
+        $prefix ??= xarDB::getPrefix();
+        $prefix = $prefix . '_calendar';
+
+        $xartables['calendar_calendar'] = $prefix . '_calendar';
+        $xartables['calendar_event'] = $prefix . '_event';
+
+        return $xartables;
+    }
+}
